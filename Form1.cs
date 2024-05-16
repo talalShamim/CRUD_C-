11a21961e252ef8d55aa56e0abeba8872b01c837
talalShamim <153133752+talalShamim@users.noreply.github.com>
2024-05-16 20:39:52 +0500

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUD_C_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=LAPTOP-D35JDB1D;Initial Catalog=CRUD_C#;Integrated Security=True;");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("insert into ut values (@id, @name, @age)", con);
+            cmd.Parameters.AddWithValue("@id",int.Parse(textBox1.Text));
+            cmd.Parameters.AddWithValue("@name", (textBox2.Text));
+            cmd.Parameters.AddWithValue("@age", double.Parse(textBox3.Text));
+
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            MessageBox.Show("Data Inserted Successfully");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=LAPTOP-D35JDB1D;Initial Catalog=CRUD_C#;Integrated Security=True;");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("update ut set name=@name, age=@age where id=@id", con);
+            cmd.Parameters.AddWithValue("@id", int.Parse(textBox1.Text));
+            cmd.Parameters.AddWithValue("@name", (textBox2.Text));
+            cmd.Parameters.AddWithValue("@age", double.Parse(textBox3.Text));
+
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            MessageBox.Show("Data Updated Successfully");
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=LAPTOP-D35JDB1D;Initial Catalog=CRUD_C#;Integrated Security=True;");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("delete ut where id=@id", con);
+            cmd.Parameters.AddWithValue("@id", int.Parse(textBox1.Text));
+
+
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            MessageBox.Show("Data Deleted Successfully");
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=LAPTOP-D35JDB1D;Initial Catalog=CRUD_C#;Integrated Security=True;");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from ut where id=@id", con);
+            cmd.Parameters.AddWithValue("@id", int.Parse(textBox1.Text));
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+
+            dataGridView1.DataSource = dt;
+
+        }
+    }
+}
